8c57297b4c081e57547096b8085a684692b5a615
Adrian Gabor <adyle5@hotmail.com>
2021-03-12 07:03:19 -0500

    added new files

---
+++ wwwroot/agblog.js
@@ -0,0 +1,27 @@
+﻿var agblog = {
+    SetLocalStorage: function (key, value) {
+        localStorage.setItem(key, JSON.stringify(value));
+    },
+
+    GetLocalStorage: function (key) {
+        return localStorage.getItem(key);
+    },
+
+    DeleteLocalStorage: function (key) {
+        localStorage.removeItem(key);
+    },
+
+    ClearLocalStorage: function () {
+        localStorage.clear();
+    },
+
+    CopyToClipboard: function (toCopy) {
+        navigator.clipboard.writeText(toCopy)
+            .then(() => {
+                console.log(toCopy + " copied to clipboard.");
+            })
+            .catch(err => {
+                console.log('Something went wrong', err);
+            });
+    }
+}
